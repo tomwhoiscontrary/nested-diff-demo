fe84d5fce35b8ac1febea3c77ef9aab930bdf6e8
Tom Anderson <twic@urchin.earth.li>
2022-09-22 16:15:09 +0100

    Baseline

---
+++ Code.java
@@ -0,0 +1,3 @@
+public class Code {
+    public static final String A_STRING = "this is a string";
+}
---
+++ TortureTest.java
@@ -0,0 +1,3 @@
+public class TortureTest {
+    public static final String HTML_PAGE = "<html><script>const htmlFragment = '<a onclick=\"alert(\\'hello from JavaScript\\')\">Hello</a>';</script></html>";
+}
